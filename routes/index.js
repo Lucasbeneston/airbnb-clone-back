8540980bc15fc08ed6305d294552165a238c8aa9
Lucas Beneston <benestonlucas@gmail.com>
2020-07-06 19:13:46 +0200

    Server start

---
+++ routes/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const router = express.Router();
+// const usersRouter = require('./users');
+
+router.use(bodyParser.json());
+// router.use(usersRouter);
+
+router.get('/', (req, res) => {
+  res.status(200).json({ message: 'Hello World!' });
+});
+
+router.get('*', (req, res) => {
+  res.status(404).json({
+    error: 'Oups ! Mauvaise route coquinou !',
+  });
+});
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const router = require('./routes');
+
+const server = express();
+
+server.use(router);
+
+const PORT = 8042;
+server.listen(PORT, () => {
+  console.log(`Le serveur est lancé sur le port ${PORT} !`);
+});
