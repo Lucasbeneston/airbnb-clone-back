0ca81d4c637d434a9b64bb96ffe9e58ed2128c94
Lucas Beneston <benestonlucas@gmail.com>
2020-07-07 09:38:42 +0200

    Server.js commentaires

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 const express = require('express');
-// server.use(cors());
-
+// const cors = require('cors');
+// const morgan = require('morgan');
 const router = require('./routes');
 
-// server.use(cors());
-// server.use(morgan('dev'));
-// server.use(router);
 const server = express();
 
 server.use('/api', router);
+// server.use(cors());
+// server.use(morgan('dev'));
+// server.use(router);
 
 const PORT = 8042;
 server.listen(PORT, () => {
@@ -17,6 +17,7 @@ server.listen(PORT, () => {
 
 module.exports = server;
 
+// // CODE A TESTER PLUS TARD
 // const express = require('express');
 // const cors = require('cors');
 // const server = express();
@@ -25,10 +26,8 @@ module.exports = server;
 
 // const router = require('./routes');
 
-// // Middleware
 // server.use(cors());
 // server.use(bodyParser.json());
 // server.use(bodyParser.urlencoded({ extended: true }));
-// // ici on appelle les routes du dossier routes
 // server.use(morgan('dev'));
 // server.use(router);
