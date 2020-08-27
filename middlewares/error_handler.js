e7264388a056b589125868a7cdd792cef5a9ed90
Lucas Beneston <benestonlucas@gmail.com>
2020-08-27 10:14:38 +0200

    Gestion erreur 500

--- middlewares/error_handler.js
+++ middlewares/error_handler.js
@@ -1,5 +1,12 @@
 module.exports = (error, request, response, next) => {
-  const { title, description } = error;
+  let { description } = error;
+  const { title } = error;
+
+  const status = error.status || 500;
+  if (status === 500) {
+    description = 'Serveur cassé. Revenez plus tard.';
+  }
+
   response.status(error.status).json({
     title,
     description,
